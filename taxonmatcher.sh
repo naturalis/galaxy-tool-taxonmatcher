8eb1bc4e388b4673da11b4312c752df2bcd80881
MartenHoogeveen <martenhoogeveen@gmail.com>
2019-05-03 11:43:53 +0000

    update

--- taxonmatcher.sh
+++ taxonmatcher.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 outlocation=$(mktemp -d /home/galaxy/galaxy/database/XXXXXX)
 SCRIPTDIR=$(dirname "$(readlink -f "$0")")
-which python3
 if [ $3 == "nsr" ]
 then
     python3 $SCRIPTDIR"/taxonmatcherV2_multicore_gbif.py" -i $1 -r $3 -t $2 -o $outlocation"/taxonmatched.txt" -n /home/galaxy/Tools/galaxy-tool-taxonmatcher/nsr_taxonmatcher
 fi
 if [ $3 == "gbif" ]
 then
-    python3 $SCRIPTDIR"/taxonmatcherV2_multicore_gbif.py" -i $1 -r $3 -t $2 -o $outlocation"/taxonmatched.txt" -g /home/galaxy/Tools/galaxy-tool-taxonmatcher/test/gbif_taxonmatcher2
+    python3 $SCRIPTDIR"/taxonmatcherV2_multicore_gbif.py" -i $1 -r $3 -t $2 -o $outlocation"/taxonmatched.txt" -g /home/galaxy/Tools/galaxy-tool-taxonmatcher/gbif_taxonmatcher
 fi
 mv $outlocation"/taxonmatched.txt" $4
 rm -rf $outlocation
--- utilities/make_nsr_database.py
+++ utilities/make_nsr_database.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sqlite3
+import csv
 db = sqlite3.connect('nsr_taxonmatcher')
 cursor = db.cursor()
 
@@ -15,17 +16,20 @@ def check_unknowns(data):
     return data
 
 def add_nsr_taxonomy():
-    with open("Taxa_tabular.txt", "r", encoding='latin-1') as nsr:#, encoding='latin-1'
+    with open("Taxa.txt", "r", encoding='latin-1') as csv_file:#, encoding='latin-1'
+        nsr = csv.reader(csv_file, delimiter=',')
         for line in nsr:
-            line = line.strip().split("\t")
+            #line = line.strip().split("\t")
             species = line[2].replace(line[3].strip(), "")
             print(line)
+            print(species)
             try:
                 metadata = line[15]+";"+line[16]+";"+line[3]
             except:
                 metadata = ""
             data = {"source":"nsr", "taxonID":line[0], "acceptedNameUsageID":line[1], "taxonomicStatus":line[4], "species_rank":species.strip(), "genus_rank":line[11].strip(), "family_rank":line[10], "order_rank":line[9],"class_rank":line[8], "phylum_rank":line[7], "kingdom_rank":line[6], "metadata":metadata}
             cursor.execute('''INSERT INTO nsr(source, taxonID, acceptedNameUsageID, taxonomicStatus, species_rank, genus_rank, family_rank, order_rank, class_rank, phylum_rank, kingdom_rank, metadata)VALUES(:source, :taxonID, :acceptedNameUsageID, :taxonomicStatus, :species_rank, :genus_rank, :family_rank, :order_rank, :class_rank, :phylum_rank, :kingdom_rank, :metadata)''', data)
+
     db.commit()
 
 
