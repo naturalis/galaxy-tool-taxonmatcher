e38c48d1213e6dbd66637919d57e71df8f504655
dickgroenenberg <dickgroenenberg@gmail.com>
2022-03-21 09:26:20 +0000

    change location gbif/nsr taxonmatcher from /media to /extend

--- taxonmatcher.sh
+++ taxonmatcher.sh
@@ -3,11 +3,15 @@ outlocation=$(mktemp -d /media/GalaxyData/database/files/XXXXXX)
 SCRIPTDIR=$(dirname "$(readlink -f "$0")")
 if [ $3 == "nsr" ]
 then
-    python3 $SCRIPTDIR"/taxonmatcherV2_multicore_gbif.py" -i $1 -r $3 -t $2 -o $outlocation"/taxonmatched.txt" -n /media/GalaxyData/blast_databases/taxonomy/nsr_taxonmatcher
+    python3 $SCRIPTDIR"/taxonmatcherV2_multicore_gbif.py" -i $1 -r $3 -t $2 -o $outlocation"/taxonmatched.txt" -n /extend/blast_databases/taxonomy/nsr_taxonmatcher
+	echo "/extend/blast_databases/taxonomy/nsr_taxonmatcher"
 fi
 if [ $3 == "gbif" ]
 then
-    python3 $SCRIPTDIR"/taxonmatcherV2_multicore_gbif.py" -i $1 -r $3 -t $2 -o $outlocation"/taxonmatched.txt" -g /media/GalaxyData/blast_databases/taxonomy/gbif_taxonmatcher
+#    python3 $SCRIPTDIR"/taxonmatcherV2_multicore_gbif.py" -i $1 -r $3 -t $2 -o $outlocation"/taxonmatched.txt" -g /media/GalaxyData/blast_databases/taxonomy/gbif_taxonmatcher
+#    echo "/media/GalaxyData/blast_databases/taxonomy/gbif_taxonmatcher"
+    python3 $SCRIPTDIR"/taxonmatcherV2_multicore_gbif.py" -i $1 -r $3 -t $2 -o $outlocation"/taxonmatched.txt" -g /extend/blast_databases/taxonomy/gbif_taxonmatcher
+    echo "/extend/blast_databases/taxonomy/gbif_taxonmatcher"
 fi
 mv $outlocation"/taxonmatched.txt" $4
 rm -rf $outlocation
